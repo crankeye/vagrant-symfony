e05093facfdba132eb17417643faa9b750352bfc
Neal Lambert <crankeye@gmail.com>
2014-03-24 15:33:55 -0700

    Check for PHPUnit before downloading and installing.

--- puppet/modules/phpunit/manifests/init.pp
+++ puppet/modules/phpunit/manifests/init.pp
@@ -4,18 +4,21 @@ class phpunit () {
       require => [ Package['curl'] ],
       cwd     => '/home',
       command => 'wget https://phar.phpunit.de/phpunit.phar',
+	  unless  => "test -f /usr/local/bin/phpunit"
    }
 
    exec { 'Permit PHPUnit':
       require => Exec['Download PHPUnit'],
       cwd     => '/home',
-      command => 'chmod +x phpunit.phar'
+      command => 'chmod +x phpunit.phar',
+	  unless  => "test -f /usr/local/bin/phpunit"
    }
 
    exec { 'Move PHPUnit':
       require => Exec['Permit PHPUnit'],
       cwd     => '/home',
       command => 'mv phpunit.phar /usr/local/bin/phpunit',
-      creates => '/usr/local/bin/phpunit'
+      creates => '/usr/local/bin/phpunit',
+	  unless  => "test -f /usr/local/bin/phpunit"
    }
 }
