8459b5472e959f1e8f58003dfe2818cdf1f7fd55
Neal Lambert <crankeye@gmail.com>
2014-04-14 16:42:47 -0700

    Update elasticsearch to version 1.1.0. Convert tab characters to 4 spaces. Change wget file to creates.

--- puppet/modules/elasticsearch/manifests/init.pp
+++ puppet/modules/elasticsearch/manifests/init.pp
@@ -1,32 +1,32 @@
 class elasticsearch (
-    $deb_url  = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.deb",
-	$deb_name = "elasticsearch-1.0.1.deb"
+    $deb_url  = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.0.deb",
+    $deb_name = "elasticsearch-1.1.0.deb"
 )
 {
-	package
-	{
-		"openjdk-7-jre-headless":
-		ensure  => present,
-		require => Exec['apt-get update']
-	}
+    package
+    {
+        "openjdk-7-jre-headless":
+        ensure  => present,
+        require => Exec['apt-get update']
+    }
 
-    exec 
+    exec
     { 
         "download-elasticsearch-deb":
             command => "/usr/bin/wget $deb_url",
-			require => Package["openjdk-7-jre-headless"],
-			unless  => "dpkg -l | grep 'ii  elasticsearch'"
+            require => Package["openjdk-7-jre-headless"],
+            creates => "/home/vagrant/$deb_name"
     }
-	
-	exec 
+
+    exec
     { 
         "install-elasticsearch-deb":
             command => "/usr/bin/dpkg -i $deb_name",
-			require => Exec["download-elasticsearch-deb"],
-			unless  => "dpkg -l | grep 'ii  elasticsearch'"
+            require => Exec["download-elasticsearch-deb"],
+            unless  => "dpkg -l | grep 'ii  elasticsearch'"
     }
-	
-	service 
+
+    service
     { 
         "elasticsearch":
             enable => true,
